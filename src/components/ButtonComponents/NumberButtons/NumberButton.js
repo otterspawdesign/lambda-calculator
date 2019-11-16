7e596872369d42943658c1171eb3e173ad016e35
Melissa Kemp <otterspawdesign@gmail.com>
2019-11-15 17:18:01 -0700

    added numbers and number buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,19 @@
 import React from "react";
-
-const NumberButton = () => {
+const NumberButton = props => {
+  const buttonClass = props.num === 0 ? "double" : null;
   return (
-    <>
+    <button
+      className={buttonClass}
+      onClick={() => {
+        props.setDisplay(
+          props.display === 0 ? props.num : "" + props.display + props.num
+        );
+      }}
+    >
+      {props.num}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,20 +1,26 @@
 import React from "react";
-
+import NumberButton from "./NumberButton";
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders.
-/* 
-import { numbers } from '../../../data' 
-*/
+import { numbers } from "../../../data";
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = props => {
   // STEP 2 - add the imported data to state
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="number-buttons">
+      {" "}
+      {numbers.map(number => {
+        number = number !== "." ? parseInt(number) : number;
+        return (
+          <NumberButton
+            num={number}
+            display={props.display}
+            setDisplay={props.setDisplay}
+          />
+        );
+      })}{" "}
     </div>
   );
 };
+
 export default Numbers;
